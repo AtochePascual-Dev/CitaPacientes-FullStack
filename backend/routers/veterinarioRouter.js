cec3ead74f57cb05da34ce48e585cc31d696a1b1
Atoche Pascual <jmatochepascual@gmail.com>
2023-07-18 10:59:15 -1000

    [FIX] solucionar la ruta para confirmar cuenta

--- backend/routers/veterinarioRouter.js
+++ backend/routers/veterinarioRouter.js
@@ -3,15 +3,18 @@ import express from 'express';
 import {
   registrarUsuario,
   confirmarCuenta,
-  autenticarUsuario
+  autenticarUsuario,
+  obtenerPerfil
 } from '../controllers/veterinarioController.js';
+import checkAuth from '../middleware/authMiddleware.js';
 
 
 const router = express.Router();
 
 // Rutas publicas
 router.post('/', registrarUsuario);
-router.get('/:token', confirmarCuenta);
+router.get('/confirmar/:token', confirmarCuenta);
 router.post('/login', autenticarUsuario);
+
 
 export default router;
